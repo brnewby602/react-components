2954cddaaa15b13df36cf5ba0bd618246dabc520
Bryan Newby <brnewby602@gmail.com>
2016-03-18 12:16:48 -0700

    Created GroceryListItems that retrieve value from props

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// TODO
+/*
+Inside app.js, create a GroceryList component that contains 
+an unordered list of 2 grocery items. Render this component 
+to div tag in index.html with an id of app
+
+*/
+
+var GroceryListItem = (props) => (<li>{props.item}</li>);
+
+
+var GroceryList = () => (<ul><GroceryListItem item={'Pace Picante Sauce'}/><GroceryListItem item={'Tortilla Chips'}/></ul>);
+
+ReactDOM.render(<GroceryList/> , document.getElementById('app'));
+
+
+
+
